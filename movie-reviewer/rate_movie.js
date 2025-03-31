8ed088142d829db7c6cb22d511d6e271860e9d8b
Joshua Argyle <joshbudd33@gmail.com>
2025-03-31 16:54:47 -0600

    Added search functionality to film inspect movie rating page.

---
+++ movie-reviewer/rate_movie.js
@@ -0,0 +1,314 @@
+const movies = [
+    {
+        title: "BACK TO THE FUTURE",
+        img: "movie_posters/back to the future.jpg",
+        rottonTomatoesRating: "93%",
+        imdbRating: "8.5/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "CINDERELLA",
+        img: "movie_posters/cinderella.jpg",
+        rottonTomatoesRating: "95%",
+        imdbRating: "7.3/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "FROZEN",
+        img: "movie_posters/frozen.jpg",
+        rottonTomatoesRating: "89%",
+        imdbRating: "7.4/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "HARRY POTTER AND THE SORCEROR'S STONE",
+        img: "movie_posters/harry potter.jpg",
+        rottonTomatoesRating: "80%",
+        imdbRating: "7.7/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "INDIANA JONES AND THE RAIDERS OF THE LOST ARK",
+        img: "movie_posters/indiana jones.jpg",
+        rottonTomatoesRating: "94%",
+        imdbRating: "8.4/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "THE LION KING",
+        img: "movie_posters/lion king.jpeg",
+        rottonTomatoesRating: "93%",
+        imdbRating: "8.5/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "NAPOLEON DYNAMITE",
+        img: "movie_posters/napoleon dynamite.jpg",
+        rottonTomatoesRating: "72%",
+        imdbRating: "7.0/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "THE PRINCE OF EGYPT",
+        img: "movie_posters/prince of egypt.jpg",
+        rottonTomatoesRating: "80%",
+        imdbRating: "7.2/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "A QUIET PLACE",
+        img: "movie_posters/quiet place.jpg",
+        rottonTomatoesRating: "96%",
+        imdbRating: "7.5/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "STAR WARS: EPISODE IV- A NEW HOPE",
+        img: "movie_posters/starwars.jpg",
+        rottonTomatoesRating: "93%",
+        imdbRating: "8.6/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "THE SUPER MARIO BROS. MOVIE",
+        img: "movie_posters/super mario bros movie.jpg",
+        rottonTomatoesRating: "59%",
+        imdbRating: "7.0/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "TENET",
+        img: "movie_posters/tenet.jpg",
+        rottonTomatoesRating: "76%",
+        imdbRating: "7.3/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "TITANIC",
+        img: "movie_posters/titanic.jpg",
+        rottonTomatoesRating: "88%",
+        imdbRating: "7.9/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+    {
+        title: "UP",
+        img: "movie_posters/up.jpeg",
+        rottonTomatoesRating: "98%",
+        imdbRating: "8.3/10",
+        action: "#.#",
+        adventure: "#.#",
+        comedy: "#.#",
+        goreRating: "#.#",
+        kidFriendly: "#.#",
+        romance: "#.#",
+        sexual: "#.#",
+        scary: "#.#",
+        suspense: "#.#",
+        religous: "#.#",
+        feelGood: "#.#",
+        },
+]
+
+function movieTemplate(movie){
+	return `
+        <div id="column_1">
+        <h2>${movie.title}</h2>
+        <img class="poster" src="${movie.img}" alt="${movie.title}">
+        <p id="rotton_tomatoes">Rotton Tomatoes Rating: ${movie.rottonTomatoesRating}</p>
+        <p id="IMDb">IMDb Rating: ${movie.imdbRating}</p></div>
+        <ul id="ratings_filtered">
+            <li>Action: ${movie.action}/10</li>
+            <li>Adventure: ${movie.adventure}</li>
+            <li>Comedy: ${movie.comedy}</li>
+            <li>Gore-Rating: ${movie.goreRating}</li>
+            <li>Kid-Friendly: ${movie.kidFriendly}</li>
+            <li>Romance: ${movie.romance}</li>
+            <li>Sexual: ${movie.sexual}</li>
+            <li>Scary: ${movie.scary}</li>
+            <li>Suspense: ${movie.suspense}</li>
+            <li>Religous: ${movie.religous}</li>
+            <li>Feel-Good: ${movie.feelGood}</li>
+        </ul>
+        <hr>`
+}
+
+function renderMovies(movieList) {
+    let movieContainer = document.querySelector('#movie');
+    let html = movieList.map(movie => movieTemplate(movie)).join("");
+    movieContainer.innerHTML += html;
+}
+function renderMovie(movieList) {
+    let movieContainer = document.querySelector('#movie');
+    let html = movieTemplate(movieList);
+    movieContainer.innerHTML += html;
+}
+
+
+function init() {
+    const sortedMovies = movies.sort((a, b) => a.title.localeCompare(b.title));
+    let movieContainer = document.querySelector('#movie');
+    movieContainer.innerHTML = '';
+    renderMovies(sortedMovies);
+}
+
+init();
+
+function filterMovies(query) {
+    query = query.toLowerCase();
+
+    const filtered = movies.filter(movie => {
+        return (
+            movie.title.toLowerCase().includes(query)
+        );
+    });
+    
+
+    return filtered.sort((a, b) => a.name.localeCompare(b.name));
+
+}
+
+
+function search_handler(event) {
+	event.preventDefault();
+   
+    const query = document.getElementById('input_box').value;
+
+    
+    const filteredMovies = filterMovies(query);
+
+    let movieContainer = document.querySelector('#movie');
+    movieContainer.innerHTML = '';
+    filteredMovies.forEach(movie => renderMovie(movie));
+}
+
+document.querySelector('#search').addEventListener('click', search_handler);
